cd98124f2aa54d9dd2bfb800ab6a9839e0816e49
sagar72427 <sagardjguns@gmail.com>
2018-07-30 18:21:45 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,2 +1,19 @@
-while True:
-	print("I am working!")
+import boto3, time, ftplib, sys, zlib, struct
+from flask import Flask, request
+from flask_restful import Resource, Api
+from ftplib import FTP
+from io import BytesIO
+import importlib as il
+
+app = Flask(__name__)
+api = Api(app)
+
+class sample(Resource):
+	def get(self):
+		return 'hello world'
+
+api.add_resource(sample, '/hello/')
+
+
+if __name__ == '__main__':
+     app.run(port='5002')
